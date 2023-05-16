e6be0d75b28a4bc07129f0bb9b2d0e9d545565f8
Mykhailo Tkachenko <20werasdf@gmail.com>
2023-05-16 22:21:49 +0300

    DONE implement course filtering

--- src/app/body/browse-courses/browse-courses.component.ts
+++ src/app/body/browse-courses/browse-courses.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {CourseStorage} from "../../core/storage";
 import {Course} from "../../core/models/course";
+import {Topics} from "../../core/models/course";
+import {Difficulties} from "../../core/models/course";
 import {HttpClient} from "@angular/common/http";
 import {Response} from "../../core/models/Response";
 import {OperationResult} from "../../core/models/OperationResult";
@@ -12,24 +12,80 @@ import {OperationResult} from "../../core/models/OperationResult";
   styleUrls: ['./browse-courses.component.scss']
 })
 export class BrowseCoursesComponent implements OnInit {
-  courses: Course[] | null = [];
-  public coursesLoading: boolean = true;
+  courses: Course[] | null | undefined = [];
+  filteredCourses: Course[] | undefined = [];
+  coursesLoading: boolean = true;
+  topicFilters: string[] = [];
+  difficulties: string[] = [];
+  topics: Record<number, string> = Topics;
+  difficultiesNames: Record<number, string> = Difficulties;
+
+  public backgrounds: Record<number, string> = {
+    1 : 'assets/courses/course-background-purple.png',
+    2 : 'assets/courses/course-background-orange.png',
+    3 : 'assets/courses/course-background-red.png',
+    4 : 'assets/courses/course-background-blue.png',
+    5 : 'assets/courses/course-background-yellow.png',
+    6 : 'assets/courses/course-background-green.png',
+  }
+
 
   constructor(private _http: HttpClient) {
   }
 
   ngOnInit() {
-    console.log('making request...')
     this._http
       .get<Response<Course[]>>('http://localhost:5233/api/courses/all')
       .subscribe(
         (response) => {
           if (response.status == OperationResult.OK) {
-            this.courses = response.content;
+            this.courses = response.content ?? [];
             this.coursesLoading = false;
             console.log(this.courses);
           }
         }
       );
+
+    this.filteredCourses = this.courses ?? [];
+  }
+
+  filterCourses() {
+    this.filteredCourses = this.courses?.filter(course => {
+      return this.topicFilters.indexOf(course.topic.toString()) !== -1 &&
+      this.difficulties.indexOf(course.difficulty.toString()) !== -1
+    });
+  }
+
+
+  onTopicFilter(element: HTMLInputElement) {
+    if (element.checked) {
+      this.topicFilters.push(element.value);
+    } else if (!element.checked) {
+      var index: number = this.topicFilters.indexOf(element.value.toString());
+
+      if (index !== -1) {
+        this.topicFilters.splice(index, 1);
+      }
+    }
+
+    this.filterCourses();
+
+    console.log(this.filteredCourses);
+  }
+
+  onDifficultyChange(element: HTMLInputElement) {
+    if (element.checked) {
+      this.difficulties.push(element.value);
+    } else if (!element.checked) {
+      var index: number = this.difficulties.indexOf(element.value);
+
+      if (index !== -1) {
+        this.difficulties.splice(index, 1);
+      }
+    }
+
+    this.filterCourses();
+
+    console.log(this.filteredCourses);
   }
 }
--- src/app/core/models/course.ts
+++ src/app/core/models/course.ts
@@ -12,11 +12,28 @@ import {CourseDifficulty} from "./CourseDifficulty";
   //password: string = '';
 //}
 
+export const Topics: Record<number, string> = {
+  0 : "None",
+  1 : "Programming",
+  2 : "Calculus",
+  3 : "Physics",
+  4 : "Chemistry",
+  5 : "English",
+  6 : "Geography"
+};
+
+export const Difficulties: Record<number, string> = {
+  0 : "None",
+  1 : "Beginner",
+  2 : "Integmediate",
+  3 : "Advanced"
+};
+
 export class Course {
   id: number = 0;
   name: string = '';
   description: string = '';
-  difficulty: CourseDifficulty = CourseDifficulty.None;
+  difficulty: number = 0;
   price: number = 0;
   topic: number = 0;
   lecturer: Lecturer | null = null;
